866be5ed05b97d5e53e87f5a3d47bf56534457a6
Mihdan Advani <mihdan31@gmail.com>
2021-11-17 06:43:41 +0700

    Tugas laravel-api uuid

---
+++ 5-2-laravel-api/app/Roles.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App;
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Model;
+
+class Roles extends Model
+{
+    // protected $table = 'roles';
+    
+    protected $fillable = ['name' , 'id'];
+
+    protected $keyType = 'string';
+
+    public $incrementing = false;
+
+    // protected static function boot()
+    // {
+    // 	parent::boot();
+
+    // 	static::creating( function($model){
+    // 		if( empty($model->id) ){
+    // 			$model->id = Str::uuid();
+    // 		}
+    // 	});
+    // }
+
+    public function users()
+    {
+        return $this->hasMany('App\Users');
+    }
+}
---
+++ 5-2-laravel-api/app/Users.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App;
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Model;
+
+class Users extends Model
+{   
+    // protected $table = 'users';
+
+        protected $fillable = [
+        'username', 'email', 'name', 'role_id', 'id'
+    ];
+
+    protected $keyType = 'string';
+
+    public $incrementing = false;
+
+    // protected static function boot()
+    // {
+    // 	parent::boot();
+
+    // 	static::creating( function($model){
+    // 		if( empty($model->id) ){
+    // 			$model->id = Str::uuid();
+    // 		}
+    // 	});
+    // }
+
+    public function roles()
+    {
+        return $this->belongsTo('App\Roles');
+    }
+}
